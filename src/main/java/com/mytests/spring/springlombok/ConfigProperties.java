4f6ee6110a2aecd06e2806f814de19f555cfaa18
irina.petrovskaya <irina.petrovskaya@jetbrains.com>
2022-06-08 10:42:55 +0200

    change from @AllArgsConstructor to @RequiredArgsConstructor

--- src/main/java/com/mytests/spring/springlombok/ConfigProperties.java
+++ src/main/java/com/mytests/spring/springlombok/ConfigProperties.java
@@ -1,14 +1,11 @@
 package com.mytests.spring.springlombok;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NonNull;
+import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
 @Getter
-@AllArgsConstructor
+@RequiredArgsConstructor
 @ConfigurationProperties(prefix = "my.foo")
 @ConstructorBinding
 public class ConfigProperties {
